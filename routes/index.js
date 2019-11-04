3a890f09f6d95f3743ebc37c87ac0cbbd4c8e3ed
Hiếu Trần <tronghieu60s@gmail.com>
2019-11-04 16:58:33 +0700

    absolute link, fix 404 css, data duplication

--- routes/index.js
+++ routes/index.js
@@ -4,56 +4,85 @@ var urlModel = require('../models/shortenlink');
 var QRCode = require('qrcode');
 var svgCaptcha = require('svg-captcha');
 
+
 router.get('/', function (req, res) {
   var captcha = svgCaptcha.create({ background: "#36393f", noise: 1, width: 100 }); //https://www.npmjs.com/package/svg-captcha
   req.session.captcha = captcha.text;
   res.render('index', { url: '', qrcode: '', urldefault: '', captcha: captcha.data, err: '' });
 });
 
-router.get('/rd/:link', function (req, res) {
+router.get('/:link', function (req, res) {
   var link = req.params.link;
   urlModel.findOne({ shorten: link }, function (err, shortURL) {
-    if(shortURL){
+    if (shortURL) {
       res.redirect(shortURL.url);
-    }else{
-      res.redirect("/404");
+    } else {
+      res.render('404');
     }
   })
 });
 
-router.get('/shorten', function (req, res) {
-  var shortPost = req.query;
-  if (req.session.captcha != shortPost.type_captcha) {
-    var captcha = svgCaptcha.create({ background: "#36393f", noise: 3, width: 100 }); //https://www.npmjs.com/package/svg-captcha
-    req.session.captcha = captcha.text;
-    res.render('index', { url: '', qrcode: '', urldefault: shortPost.url, captcha: captcha.data, err: 'Incorrect captcha code!' });
+router.get('/create/shorten', function (req, res) {
+  if (isEmpty(req.query)) {
+    res.render('404');
   } else {
-    var controlShortenURL = () => {
-      if (shortPost.linkmode == 1) {
-        return randomCharZeroWidth(shortPost.charlength);
-      } else if (shortPost.linkmode == 2) {
-        return randomNumbers(shortPost.charlength);
-      } else {
-        return randomChar(shortPost.charlength);
+    var shortPost = req.query;
+    if (req.session.captcha != shortPost.type_captcha) {
+      var captcha = svgCaptcha.create({ background: "#36393f", noise: 3, width: 100 }); //https://www.npmjs.com/package/svg-captcha
+      req.session.captcha = captcha.text;
+      res.render('index', { url: '', qrcode: '', urldefault: shortPost.url, captcha: captcha.data, err: 'Incorrect captcha code!' });
+    } else {
+      var shortenURL = controlShortenURL(shortPost.linkmode, shortPost.charlength);
+      var dataURL = {
+        url: shortPost.url,
+        shorten: shortenURL
       }
+      urlExist(shortenURL, shortPost.linkmode, shortPost.charlength, () => {
+        QRCode.toDataURL(shortPost.url, function (err, qrcode) {
+          urlModel.create(dataURL, function (err, small) {
+            if (!err) {
+              var captcha = svgCaptcha.create({ background: "#36393f", noise: 3, width: 100 }); //https://www.npmjs.com/package/svg-captcha
+              req.session.captcha = captcha.text;
+              res.render('index', { url: shortenURL, qrcode: qrcode, urldefault: shortPost.url, captcha: captcha.data, err: '' });
+            }
+          });
+        })
+      })
     }
-    var shortenURL = controlShortenURL();
-    var dataURL = {
-      url: shortPost.url,
-      shorten: shortenURL
-    }
-    QRCode.toDataURL(shortPost.url, function (err, qrcode) {
-      urlModel.create(dataURL, function (err, small) {
-        if (!err) {
-          var captcha = svgCaptcha.create({ background: "#36393f", noise: 3, width: 100 }); //https://www.npmjs.com/package/svg-captcha
-          req.session.captcha = captcha.text;
-          res.render('index', { url: shortenURL, qrcode: qrcode, urldefault: shortPost.url, captcha: captcha.data, err: '' });
-        }
-      });
-    })
   }
 })
 
+function controlShortenURL(linkMode, charLength) {
+  if (linkMode == 1) {
+    return randomCharZeroWidth(10);
+  } else if (linkMode == 2) {
+    return randomNumbers(charLength);
+  } else {
+    return randomChar(charLength);
+  }
+}
+
+/* Check URL exist */
+function urlExist(shortenURL, linkMode, charLength, cb) {
+  urlModel.findOne({ shorten: shortenURL }, function (err, shortURL) {
+    if (shortURL) {
+      shortenURL = controlShortenURL(linkMode, charLength);
+      urlExist(shortenURL, linkMode, charLength, cb);
+    } else {
+      cb();
+    }
+  })
+}
+/* Check Obj Empty */
+function isEmpty(obj) {
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key))
+      return false;
+  }
+  return true;
+}
+
+/* Random Character, Number and Zero Width */
 function randomChar(length) {
   var result = '';
   var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -63,7 +92,6 @@ function randomChar(length) {
   }
   return result;
 }
-
 function randomNumbers(num) {
   var result = '';
   for (let i = 0; i < num; i++) {
@@ -71,7 +99,6 @@ function randomNumbers(num) {
   }
   return result;
 }
-
 function randomCharZeroWidth(num) {
   var zeroWidth = ['\u200c', '\u200d'];
   var result = '';
