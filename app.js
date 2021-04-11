79978b77b1569d84554e155df9670ae7ec651acf
Trọng Hiếu <tronghieu60s@gmail.com>
2021-04-11 16:31:53 +0700

    update

--- app.js
+++ app.js
@@ -1,55 +1,59 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const logger = require("morgan");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.set('trust proxy', 1) // trust first proxy
-app.use(session({
-  secret: '43271843240247236984',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 60000 }
-}))
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("trust proxy", 1); // trust first proxy
+app.use(
+  session({
+    secret: "43271843240247236984",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 },
+  })
+);
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
+const indexRouter = require("./routes/index");
+app.use("/", indexRouter);
 
 //Mongo Database
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/myproject', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
-});
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/myproject", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("✔️ Connected Database SuccessFul!"))
+  .catch((err) => console.log(`❌ Connect Database Failed!\n ${err}`));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.render("404");
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,57 +1,96 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const urlModel = require('../models/shortenlink');
-const QRCode = require('qrcode');
-const svgCaptcha = require('svg-captcha');
+const urlModel = require("../models/shortenlink");
+const QRCode = require("qrcode");
+const svgCaptcha = require("svg-captcha");
 
-const { isEmpty, controlShortenURL } = require('./support');
+const { isEmpty, controlShortenURL } = require("./support");
 
-router.get('/', function (req, res) {
-  var captcha = svgCaptcha.create({ background: "#36393f", noise: 1, width: 100 }); //https://www.npmjs.com/package/svg-captcha
+router.get("/", function (req, res) {
+  const captcha = svgCaptcha.create({
+    background: "#36393f",
+    noise: 1,
+    width: 100,
+  }); //https://www.npmjs.com/package/svg-captcha
   req.session.captcha = captcha.text;
-  res.render('index', { url: '', qrcode: '', urldefault: '', captcha: captcha.data, err: '' });
+
+  const renderData = {
+    url: "",
+    qrcode: "",
+    urldefault: "",
+    captcha: captcha.data,
+    err: "",
+  };
+  res.render("index", renderData);
 });
 
-router.get('/:link', function (req, res) {
+router.get("/:link", function (req, res) {
   var { link } = req.params;
   urlModel.findOne({ shorten: link }, function (err, shortURL) {
     if (shortURL) {
       res.redirect(shortURL.url);
     } else {
-      res.render('404');
+      res.render("404");
     }
-  })
+  });
 });
 
-router.get('/create/shorten', function (req, res) {
+router.get("/create/shorten", function (req, res) {
   if (isEmpty(req.query)) {
-    res.render('404');
-  } else {
-    var shortPost = req.query;
-    if (req.session.captcha != shortPost.type_captcha) {
-      var captcha = svgCaptcha.create({ background: "#36393f", noise: 3, width: 100 }); //https://www.npmjs.com/package/svg-captcha
-      req.session.captcha = captcha.text;
-      res.render('index', { url: '', qrcode: '', urldefault: shortPost.url, captcha: captcha.data, err: 'Incorrect captcha code!' });
-    } else {
-      var shortenURL = controlShortenURL(shortPost.linkmode, shortPost.charlength);
-      var dataURL = {
-        url: shortPost.url,
-        shorten: shortenURL
-      }
-      urlExist(shortenURL, shortPost.linkmode, shortPost.charlength, () => {
-        QRCode.toDataURL(shortPost.url, function (err, qrcode) {
-          urlModel.create(dataURL, function (err, small) {
-            if (!err) {
-              var captcha = svgCaptcha.create({ background: "#36393f", noise: 3, width: 100 }); //https://www.npmjs.com/package/svg-captcha
-              req.session.captcha = captcha.text;
-              res.render('index', { url: shortenURL, qrcode: qrcode, urldefault: shortPost.url, captcha: captcha.data, err: '' });
-            }
-          });
-        })
-      })
-    }
+    res.render("404");
+    return;
   }
-})
+  
+  const shortPost = req.query;
+  if (req.session.captcha != shortPost.type_captcha) {
+    var captcha = svgCaptcha.create({
+      background: "#36393f",
+      noise: 3,
+      width: 100,
+    }); //https://www.npmjs.com/package/svg-captcha
+    req.session.captcha = captcha.text;
+
+    const renderData = {
+      url: "",
+      qrcode: "",
+      urldefault: shortPost.url,
+      captcha: captcha.data,
+      err: "Incorrect captcha code!",
+    };
+    res.render("index", renderData);
+    return;
+  }
+
+  const shortenURL = controlShortenURL(
+    shortPost.linkmode,
+    shortPost.charlength
+  );
+  const dataURL = {
+    url: shortPost.url,
+    shorten: shortenURL,
+  };
+  urlExist(shortenURL, shortPost.linkmode, shortPost.charlength, () => {
+    QRCode.toDataURL(shortPost.url, function (err, qrcode) {
+      urlModel.create(dataURL, function (err, small) {
+        if (!err) {
+          var captcha = svgCaptcha.create({
+            background: "#36393f",
+            noise: 3,
+            width: 100,
+          }); //https://www.npmjs.com/package/svg-captcha
+          req.session.captcha = captcha.text;
+          res.render("index", {
+            url: shortenURL,
+            qrcode: qrcode,
+            urldefault: shortPost.url,
+            captcha: captcha.data,
+            err: "",
+          });
+        }
+      });
+    });
+  });
+});
 
 /* Check URL exist */
 function urlExist(shortenURL, linkMode, charLength, cb) {
@@ -62,7 +101,7 @@ function urlExist(shortenURL, linkMode, charLength, cb) {
     } else {
       cb();
     }
-  })
+  });
 }
 
 module.exports = router;
